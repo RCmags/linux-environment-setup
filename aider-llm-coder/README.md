e24cddf42019c7272144434ca355ee73283d1579
RCmags <memoryofatrufestival@gmail.com>
2026-06-23 02:38:47 -0500

    install instructions for aider llm

---
+++ aider-llm-coder/README.md
@@ -0,0 +1,3 @@
+# Aider AI chat setup
+This subfolder contains the setup files to run the aider-ai coding assistant
+with a local LLM model. 
---
+++ aider-llm-coder/aider-setup.sh
@@ -0,0 +1,22 @@
+# install aider 
+# See: https://aider.chat/docs/install.html
+sudo apt install pipx
+pipx install aider-install
+
+# copy config file to user folder
+cp .aider.conf.yml ~/.aider.conf.yml
+
+# Install ollama
+curl -fsSL https://ollama.com/install.sh | sh
+
+# NOTE: Use efficient CPU-based models with mixture of experts.
+ollama pull lfm2.5:latest
+
+# Add the API base:
+nano ~/.bashrc
+
+# append to bottom of the file:
+#export OLLAMA_API_BASE=http://127.0.0.1:11434
+
+echo 'export OLLAMA_API_BASE=http://127.0.0.1:11434' >> ~/.bashrc
+source ~/.bashrc
